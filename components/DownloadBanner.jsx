e9cdee228a320a47e992e7f2565f15c30e0fb1ee
Conion Developer <shahazeem.ahmed.software.engineer@gmail.com>
2026-03-28 02:24:55 +0530

    update apk name

--- components/DownloadBanner.jsx
+++ components/DownloadBanner.jsx
@@ -25,7 +25,7 @@ const DownloadBanner = () => {
       </div>
       <a
         href="/app/contextsplit-v1.apk"
-        download="ContextSplit.apk"
+        download="contextsplit-v1.apk"
         className="btn-purple text-xs px-4 py-2 w-full sm:w-auto text-center"
       >
         Download APK
